1b2da226e438b752572c568f3513867b80dcbee9
Gen1usG <812487021@qq.com>
2020-05-28 23:19:15 +0800

    deploy

---
+++ precache-manifest.ad18e0d9c471f588068ca018875e7a1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1fa0976e9fd1e5304ff",
+    "url": "/ledger-website/css/app.bb9ad7f8.css"
+  },
+  {
+    "revision": "60f23ff7cbbf6979764c310aa524210b",
+    "url": "/ledger-website/img/QRCode.png"
+  },
+  {
+    "revision": "0245c5093279278ae0126d704bc762ff",
+    "url": "/ledger-website/index.html"
+  },
+  {
+    "revision": "b1fa0976e9fd1e5304ff",
+    "url": "/ledger-website/js/app.8e329f13.js"
+  },
+  {
+    "revision": "ab93542c5c1c2f89a739",
+    "url": "/ledger-website/js/chunk-vendors.fc3ebc99.js"
+  },
+  {
+    "revision": "fecdb3691605218d383bb0937c45e085",
+    "url": "/ledger-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ledger-website/robots.txt"
+  }
+]);
